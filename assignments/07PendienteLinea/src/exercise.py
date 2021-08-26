565b8f6e9dac0f2eda524dae87fbd09f6ed21267
aquae <A01412614@itesm.mx>
2021-08-26 10:21:18 -0500

    Done

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,7 +1,13 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    x1 = int(input('Dame el valor de x1: '))
+    y1 = int(input('Dame el valor de y1: '))
+    x2 = int(input('Dame el valor de x2: '))
+    y2 = int(input('Dame el valor de y2: '))
 
+    m = (y2-y1) / (x2-x1)
+
+    print('El valor de la pendiente es igual a:', m)
 
 if __name__ == '__main__':
     main()
