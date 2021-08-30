7b9c952a6a2a5b439d6d0b526c47556652388d1e
aquae <A01412614@itesm.mx>
2021-08-29 22:51:20 -0500

    Done

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    harina = float(input('Dame la cantidad de harina em gramos: '))
+
+    gramos_lev = (harina * 50) / 1000
+
+    print('Necesitas', gramos_lev, 'gramos de levadura')
 
 
 if __name__ == '__main__':
