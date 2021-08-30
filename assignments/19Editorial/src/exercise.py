d630a56dbd77ac448eea7e72eaefdf4c24bc7250
aquae <A01412614@itesm.mx>
2021-08-29 22:47:50 -0500

    Done

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,17 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+
+    no_palabras = int(input('Dame el número de palabras: '))
+
+    if no_palabras < 475
+        print('El costo total es de 54 dólares')
+
+    else:
+        no_paginas = no_palabras / 475
+        costo = (no_paginas * 60)
+        costo_total = costo - (costo * 0.9)
+        print('El costo total es de:', costo_total)
+
 
 
 if __name__ == '__main__':
